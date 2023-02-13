f41248d9286e976483ddd23ce13ce20d63b132dc
Fredson Silva <silva.fredson@hotmail.com>
2023-02-13 13:54:52 -0300

    Initial Commit

---
+++ DeepSleep/DeepSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepSleepApp.swift
+//  DeepSleep
+//
+//  Created by Fredson Silva on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DeepSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
